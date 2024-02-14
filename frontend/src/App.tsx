f9a2110cdd8867ede03511a2ebd1befb4beaea13
arse0039 <arse0039@umn.edu>
2024-02-13 17:34:28 -0800

    Updated routes to be state based instead of route based for cleaner rendering. Updated CSS for about Page and nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
 import { NavBar } from './components/NavBar';
 import { navLinks } from './constants/data';
-import AboutPage from './pages/aboutPage';
 import HomePage from './pages/homePage';
+import AboutPage from './pages/aboutPage';
 import ProjectPage from './pages/projectPage';
 
-
 const App: React.FC = () => {
+  const [displayedPage, setDisplayedPage] = useState<string>('home');
+
+  const handleLinkClick = (page:string) => {
+    setDisplayedPage(page);
+  };
+
   return (
-    <div>
-      <NavBar navArray={navLinks}/>
-      <Router>
-        <Routes>
-          <Route path='/'  Component={HomePage} />
-          <Route path='/home'  Component={HomePage} />
-          <Route path='/about'  Component={AboutPage} />
-          <Route path='/projects'  Component={ProjectPage} />
-        </Routes>
-      </Router>
-    </div>
+      <div>
+        <NavBar navArray={navLinks} handleClick={handleLinkClick}/>
+        {displayedPage === 'Home' && <HomePage/>}
+        {displayedPage === 'About' && <AboutPage/>}
+        {displayedPage === 'Projects' && <ProjectPage/>}
+      </div>
   );
 }
 
