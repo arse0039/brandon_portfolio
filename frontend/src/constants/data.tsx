f1d8b5bdf02f43613e1f517fb5c838bfb65d0b1e
arse0039 <arse0039@umn.edu>
2024-02-18 17:58:29 -0800

    Removed icons from NavBar array

--- frontend/src/constants/data.tsx
+++ frontend/src/constants/data.tsx
@@ -1,16 +1,10 @@
-import { GiBookshelf } from "react-icons/gi";
-import { HiOutlineHome } from "react-icons/hi";
-import { BsPersonArmsUp } from "react-icons/bs";
-import React, { ReactElement } from "react";
-
 export interface NavLink {
     name:string;
-    icon:ReactElement;
 }
 
 export const navLinks:NavLink[] = [
-    {name:'Home', icon: <HiOutlineHome/>}, 
-    {name:'About', icon: <BsPersonArmsUp/>}, 
-    {name:'Projects', icon: <GiBookshelf/>},
+    {name:'Home'}, 
+    {name:'About'}, 
+    {name:'Projects'},
 ];
     
