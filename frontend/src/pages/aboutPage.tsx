a0494f6236c2192240f97ec4c901db76780b3e61
arse0039 <arse0039@umn.edu>
2024-02-18 17:48:13 -0800

    Fixed import issue

--- frontend/src/pages/aboutPage.tsx
+++ frontend/src/pages/aboutPage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../styles/aboutPage.css';
 import brandonImage from '../assets/BArsenault.jpg';
 import davidImage from '../assets/DClaphan.jpg';
-import AboutModal from '../components/AboutModal';
+import AboutModal from '../components/aboutModal';
 
 interface AboutPageProps {
     handleModalClick: () => void;
