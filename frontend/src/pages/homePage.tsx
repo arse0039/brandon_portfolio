6f103dd81b5a30675791976a2388494c4183b9c9
arse0039 <arse0039@umn.edu>
2024-03-01 15:17:06 -0800

    Updated and styled home page text

--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -7,11 +7,17 @@ const HomePage: React.FC = () => {
       <div className="home-page">
         <AnimatedBackground /> 
         <div className="content">
-          <h1>Welcome to our website!</h1>
-          <p> We are best buds and we went to school together. </p>
-          <p> Now we are sad because we can't find jobs. </p>
-          <p> Hire us, please.</p>
-          <p> Thanks! </p>
+          <h1>Welcome!</h1>
+          <div className='home-paragraph'>
+            We are Brandon and David, two <span className="rotating-text_1">career-changers </span>
+            turned coding enthusiasts who found their passion and purpose for software engineering. <br/>
+            We met early during our time at Oregon State and quickly became friends.  <br/>
+            Our satisfaction comes not just from solving problems but from the joy of 
+            collaborative innovation. <br/>
+            We are excited that you are here to explore our journey. <br/>
+            As we continue to grow and learn, so does this space. <br/>
+            Join us as we continue to build extraordinary things together! <br/>
+          </div>
         </div>
       </div>
     );
